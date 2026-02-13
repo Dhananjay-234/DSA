31c79da98d0e12c70201f01f1019385b4e145e25
Your Name <you@example.com>
2026-02-13 14:17:39 +0530

    sum function used

--- functions.cpp
+++ functions.cpp
@@ -1,8 +1,25 @@
 #include<iostream>
 using namespace std;
 
-int main(){
-    
+// void printName(string name){
+//     cout<<"Dhananjay "<<name;
 
+// }
+// int main(){
+//     string name;
+//     cin>>name;
+//     printName(name);
+//     return 0;
+// }
+
+void sum(int num1, int num2){
+    int num3=num1+num2;
+    cout<<num3;
+
+}
+int main(){
+    int num1, num2;
+    cin>>num1>>num2;
+    sum(num1, num2);
     return 0;
 }
