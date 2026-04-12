b6bccf8620cab63f1c7d80261484398011d660b0
Your Name <shindedhananjay2304@gmail.com>
2026-04-13 00:54:23 +0530

    Printed Numbers In reverse order using recursion

--- PrintNumInRev.java
+++ PrintNumInRev.java
@@ -1,15 +1,26 @@
 import java.util.*;
 public class PrintNumInRev {
+//    static void f(int i,int n){
+//        if (i>n)
+//            return;
+//        System.out.println(n);
+//        f(i,n-1);
+//    }
+//    public static void main(String[] args){
+//        Scanner sc=new Scanner(System.in);
+//        int n=sc.nextInt();
+//
+//        f(1,n);
+//    }
     static void f(int i,int n){
-        if (i>n)
+        if(i<1)
             return;
-        System.out.println(n);
-        f(i,n-1);
+        System.out.println(i);
+        f(i-1,n);
     }
     public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-
-        f(1,n);
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt() ;
+        f(n,n);
     }
 }
