01bb21e11b5d64bfa1104cf916af78fb7bf2aa09
Your Name <shindedhananjay2304@gmail.com>
2026-04-14 00:34:44 +0530

    sum of numbers using recursion

---
+++ SumOfFirstNnum.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class SumOfFirstNnum {
+    static void  f(int i, int sum){
+        if (i<1){
+            System.out.println(sum);
+                return;
+        }
+
+        f(i-1,sum+i);
+    }
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        int n= sc.nextInt();
+
+        f(n,0);
+    }
+}
