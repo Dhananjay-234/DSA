3e51fc87798a616792adbe6774d655d93c5e4901
Your Name <shindedhananjay2304@gmail.com>
2026-04-14 15:41:37 +0530

    String palindrome checked using recursion

---
+++ StringPalindromeCheck.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class StringPalindromeCheck {
+    static boolean f(int i, String s){
+        if(i>=s.length()/2)
+            return true;
+        if(s.charAt(i)!=s.charAt(s.length()-i-1))
+            return false;
+        return f(i+1,s);
+
+    }
+    public static void main(String[] args){
+        String s="madam";
+        System.out.println(f(0,s));
+    }
+}
