a9be4fd5bcb174d48c94214c342973a2d875200a
Your Name <shindedhananjay2304@gmail.com>
2026-04-16 17:42:39 +0530

    array in java

---
+++ arrays.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class arrays {
+    public static void main(String[] args){
+        int arr[]=new int[5];
+        int n=arr.length;
+        Scanner sc=new Scanner(System.in);
+
+        for (int i=0;i<n;i++){
+            System.out.println("provide inout for index "+i);
+            arr[i]=sc.nextInt();
+        }
+
+        for(int val:arr){
+            System.out.println(val);
+        }
+    }
+}
