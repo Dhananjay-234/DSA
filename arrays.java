ee79dcbf4d3a1dcdcac86fff4ad914985c16eabe
Your Name <shindedhananjay2304@gmail.com>
2026-04-17 23:57:57 +0530

    array in java

--- arrays.java
+++ arrays.java
@@ -2,17 +2,19 @@ import java.util.Scanner;
 
 public class arrays {
     public static void main(String[] args){
-        int arr[]=new int[5];
-        int n=arr.length;
-        Scanner sc=new Scanner(System.in);
 
-        for (int i=0;i<n;i++){
-            System.out.println("provide inout for index "+i);
-            arr[i]=sc.nextInt();
-        }
-
-        for(int val:arr){
-            System.out.println(val);
-        }
-    }
+        //        int arr[]=new int[5];
+//        int n=arr.length;
+//        Scanner sc=new Scanner(System.in);
+//
+//        for (int i=0;i<n;i++){
+//            System.out.println("provide inout for index "+i);
+//            arr[i]=sc.nextInt();
+//        }
+//
+//        for(int val:arr){
+//            System.out.println(val);
+//        }
+//    }
 }
+
