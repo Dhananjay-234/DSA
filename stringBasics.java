49b48b3caa4b05647bf4fcdd95f9b129a35bec52
Your Name <shindedhananjay2304@gmail.com>
2026-04-18 17:49:44 +0530

    basics string op

---
+++ stringBasics.java
@@ -0,0 +1,10 @@
+public class stringBasics {
+    public static void main(String[] args){
+    String firstName="Dhananjay";
+    String lastName="Shinde";
+
+    System.out.println(firstName+" "+lastName);
+    System.out.println(firstName.length());
+    System.out.println(lastName.charAt(4));
+    }
+}
