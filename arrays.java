33e93fd8419d6a207a212b25e4ae1480c2323926
Your Name <shindedhananjay2304@gmail.com>
2026-04-18 13:59:28 +0530

    find Max val in 2D array

--- arrays.java
+++ arrays.java
@@ -2,18 +2,33 @@ import java.util.Scanner;
 
 public class arrays {
     public static void main(String[] args) {
-        int[][] arr={
-                {1,2,3},{4,5,6},{7,8,9}
-        };
+        int[][] arr={ {1,2,3},{4,5,6},{7,8,9} };
+        int maxVal=arr[0][0];
+        for (int i=0;i<arr.length;i++){
+            for (int j=0;j<arr[i].length;j++){
+                if (arr[i][j]>maxVal){
+                    maxVal=arr[i][j];
+                }
 
-        int mult=1;
-        for (int i=0;i< arr.length;i++){
-            for(int j=0;j<arr[i].length;j++){
-                mult=mult*arr[i][j];
             }
         }
-        System.out.println("Multiplication of all numbers from 2 D array is:"+mult);
+        System.out.println("maximum val in 2D array is :"+maxVal);
 
+
+
+
+//        int[][] arr={
+//                {1,2,3},{4,5,6},{7,8,9}
+//        };
+//
+//        int mult=1;
+//        for (int i=0;i< arr.length;i++){
+//            for(int j=0;j<arr[i].length;j++){
+//                mult=mult*arr[i][j];
+//            }
+//        }
+//        System.out.println("Multiplication of all numbers from 2 D array is:"+mult);
+//
 
 
 //    int[][] arr={
